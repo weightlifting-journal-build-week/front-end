a572f071e3698484fb7705dc47e68ecc5203d542
Timothy Shores <timothymshores@gmail.com>
2019-04-16 11:16:08 -0500

    Added Joe\'s initial code from Login.js into MaterialUILogin component

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import reducer from './redux/reducers'; 
+import reducer from './redux/reducers';
 import App from './App';
+
+import './index.css';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
-  
+
     <Provider store={store}>
-      <App /> 
+        <App />
     </Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
